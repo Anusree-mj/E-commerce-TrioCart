fdcf0e22816a565685273b67214a670d30e1c1fa
Anusree <anu.mj0608@gmail.com>
2023-11-08 22:33:53 +0530

    data connenction

--- routes/mongodb.js
+++ routes/mongodb.js
@@ -1,40 +1,53 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose');
 
 mongoose.connect("mongodb://localhost:27017/TrioCart")
-.then(()=>{
-    console.log("mongodb connected")
-}).catch(()=>{
-    console.log('failed to connect')
-})
-const ProductSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+    .then(() => {
+        console.log("mongodb connected");
+    })
+    .catch(() => {
+        console.log('failed to connect');
+    });
+
+const ProductSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        detailed_description: {
+            type: String,
+            required: true,
+        },
+        category: {
+            type: String,
+            required: true,
+        },
+        subCategory: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: String,
+            required: true,
+        },
+        size: {
+            type: Array,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: true,
+        },
     },
-    description:{
-        type:String,
-        required:true
-    },
-    category:{
-        type:String,
-        required:true
-    },
-    subCategory:{
-        type:String,
-        required:true
-    },
-    price:{
-        type:Number,
-        required:true
-    },
-    size:{
-        type:Array,
-        required:true
-    },
-    image:{
-        type:String,
-        required:true
-    },
-})
-const productsCollection= new mongoose.model("products",ProductSchema)
-module.exports={productsCollection}
+    {
+        timestamps: true, // This option adds createdAt and updatedAt timestamps
+    }
+);
+
+const productsCollection = new mongoose.model("products", ProductSchema);
+
+module.exports = { productsCollection };
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var productHelpers=require('../helpers/product-helpers')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home');
+  productHelpers.getAllProducts().then((product1,product2,product3,newProducts)=>{
+    // console.log(product)
+    res.render('users/home', { layout: 'layout/layout',product1,product2,product3,newProducts });
+   })
 });
 
 module.exports = router;
